09749d7882d9d8db7f728036b805bd712e45a25f
JR506 <jawaherh.0050@gmail.com>
2024-08-26 22:23:25 +0300

    homeWork

---
+++ HomeWorkSwiftUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HomeWorkSwiftUI
+//
+//  Created by JR on 22/02/1446 AH.
+//
+
+import SwiftUI
+
+struct Item: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Item()
+}
